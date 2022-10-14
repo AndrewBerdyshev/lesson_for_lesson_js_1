9d5a2e08cd02faa605f312b0cf1c1ec513241915
AndrewBerdyshev <an.berdyshev@gmail.com>
2022-10-14 16:20:34 +0700

    last commit

---
+++ index.js
@@ -0,0 +1,41 @@
+// task 1
+// var uName = prompt("Enter your name");
+// alert(`Hello, ${uName}!`);
+
+// task 2
+// var uAge = Number(prompt("Enter your birth day"));
+// const current_year = 2022;
+// alert(`You're age is ${current_year - uAge}`);
+
+// task 3
+// var sSide = Number(prompt("enter the side of the square"));
+// alert(`Square perimeter is ${sSide*4}`);
+
+// task 4
+// var cRadius = Number(prompt("Enter the radius of the circle"));
+// alert(`Area of this circle is ${3.14 * cRadius ** 2}`);
+
+// task 5
+// var distance = Number(prompt("Enter the distance between cities"));
+// var time = Number(prompt("Enter the needing time"));
+// alert(`Needing speed: ${distance/time}`);
+
+// task 6
+// var dollars = Number(prompt("Enter dollar's count"));
+// alert(`Euro: ${dollars * 1.0144}`);
+
+// task 7
+// var size = Number(prompt("Enter flash driver size"));
+// alert(`Can contain ${Math.floor((size * 1024 / 820))} 820mb files`);
+
+// task 8
+// var sum = Number(prompt("Enter the amount"));
+// alert(`You can buy ${Math.floor(sum / 100)} chocolate bars and the change will be ${sum % 100}`);
+
+// task 9
+// var num = prompt("Enter num");
+// alert(`${num[2]}${num[1]}${num[0]}`);
+
+// task 10
+// var num = prompt("Enter number");
+// alert(["Even", "Odd"][num%2]);
